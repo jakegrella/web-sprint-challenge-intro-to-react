81aa3fbd819431025965da7188b8e2be0a0601bf
Jake Grella <jgrella13@gmail.com>
2020-10-09 12:33:38 -0500

    completed mvp

--- src/App.js
+++ src/App.js
@@ -37,10 +37,16 @@ export default function App() {
 	// console.log('chars1', characters);
 	return (
 		<div className='App'>
+			<img
+				src='https://upload.wikimedia.org/wikipedia/en/c/c8/Rick_and_Morty_logo.png'
+				alt='rick and morty logo'
+			/>
 			<h1 className='Header'>Characters</h1>
-			{characters.map((char) => {
-				return <Character key={char.id} char={char} />;
-			})}
+			<div className='container'>
+				{characters.map((char) => {
+					return <Character key={char.id} char={char} />;
+				})}
+			</div>
 		</div>
 	);
 }
---
+++ src/styles/theme/index.js
@@ -0,0 +1,10 @@
+export default {
+	primaryColor: '#FC3D21',
+	secondaryColor: '#0B3D91',
+	black: '#212121',
+	white: '#fff',
+	breakpoints: {
+		small: '(max-width: 650px)',
+		medium: '(max-width: 1250px)',
+	},
+};
