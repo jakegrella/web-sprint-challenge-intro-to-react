0b52006c6a2e700a26f79659caa0a0e780d4b866
Jake Grella <jgrella13@gmail.com>
2020-10-09 11:38:17 -0500

    axios working

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
 // Write your Character component here
+import React from 'react';
+import Details from './Details';
+
+export default function Character(props) {
+	return (
+		<div className='character'>
+			{props.char.id}
+			{props.char.name}
+			<Details char={props.char} />
+			{/* <button onClick={() => openDetails(props.info.id)}>See Details</button> */}
+		</div>
+	);
+}
